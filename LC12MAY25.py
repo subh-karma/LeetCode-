d9479aeef136ff5eb6d89f57bffa6176a4684048
Subhasish Karmakar <75238941+subh-karma@users.noreply.github.com>
2025-05-12 07:21:37 +0530

    Create LC12MAY25.py

---
+++ LC12MAY25.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findEvenNumbers(self, digits: List[int]) -> List[int]:
+        ans = []
+        freq = Counter(digits)
+        for x in range(100, 1000, 2): 
+            if not Counter(int(d) for d in str(x)) - freq: ans.append(x)
+        return ans 
