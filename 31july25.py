54bc8f925cfdeaf7d3e6680f35286ebb8ed6e664
Subhasish Karmakar <75238941+subh-karma@users.noreply.github.com>
2025-07-31 22:09:23 +0530

    Create 31july25.py

---
+++ 31july25.py
@@ -0,0 +1,9 @@
+class Solution:
+    def subarrayBitwiseORs(self, arr):
+        res = set()
+        cur = set()
+        for num in arr:
+            cur = {num | x for x in cur} | {num}
+            res |= cur
+        return len(res)
+        
