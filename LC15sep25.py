e518ca6c59f836e28f36e9276fd0edfb9a205f98
Subhasish Karmakar <75238941+subh-karma@users.noreply.github.com>
2025-09-15 21:36:07 +0530

    Create LC15sep25.py

---
+++ LC15sep25.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canBeTypedWords(self, text: str, broken: str) -> int:
+        brokenKeys = set(broken)
+        words = text.split(" ")
+        count = 0
+
+        for word in words:
+            for c in word:
+                if c in brokenKeys:
+                    count += 1
+                    break
+
+        return len(words) - count
+        
