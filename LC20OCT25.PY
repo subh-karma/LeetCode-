9b01c5e1061220d07dda067d4d3bb098e0f6e31b
Subhasish Karmakar <75238941+subh-karma@users.noreply.github.com>
2025-10-20 22:24:12 +0530

    Create LC20OCT25.PY

---
+++ LC20OCT25.PY
@@ -0,0 +1,11 @@
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        res = 0
+        for o in operations:
+            if "+" == o[1]:
+                res += 1
+            else:
+                res -= 1
+        
+        return res
+        
