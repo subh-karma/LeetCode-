508a0ee7a7ef34e28d46a01f6485f81312fe1563
Subhasish Karmakar <75238941+subh-karma@users.noreply.github.com>
2026-04-20 21:37:48 +0530

    Implement maxDistance method in Solution class

---
+++ LC20Apr26.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxDistance(self, colors: List[int]) -> int:
+        
+        n = len(colors)
+
+        for i in range(n):
+            if (colors[i] != colors[~0]) or (colors[~i] != colors[0]):
+                return n - (i + 1)
+    
+        
