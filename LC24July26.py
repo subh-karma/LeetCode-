c0acfd86effd5786f099ac87f567f031d348f781
Subhasish Karmakar <75238941+subh-karma@users.noreply.github.com>
2026-07-24 22:13:34 +0530

    Implement uniqueXorTriplets method in Solution class

---
+++ LC24July26.py
@@ -0,0 +1,16 @@
+class Solution:
+    def uniqueXorTriplets(self, nums: list[int]) -> int:
+
+        update = lambda x: num^x
+
+        pairs, trips = set([0]), set(nums),  
+        digits = 2** max(nums).bit_length()
+
+        while nums:
+            num = nums.pop()
+            trips|= set(map(update, pairs))
+            pairs|= set(map(update, nums))
+            if len(trips) == digits: break
+ 
+        return len(trips)
+        
